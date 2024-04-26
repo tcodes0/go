d327bb05b4ba042f16773bd7f6f51bcc9d1880c7
Thom Ribeiro <thomazella9@gmail.com>
2024-04-26 13:55:57 -0300

    renames, check for http.Flusher without panic

--- src/httpflush/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/tcodes0/go/httpflush
-
-go 1.22
---
+++ src/httpflush/max_size.go
@@ -0,0 +1,44 @@
+package httpflush
+
+import (
+	"errors"
+	"net/http"
+)
+
+var ErrWriterNotFlusher = errors.New("http.ResponseWriter does not implement http.Flusher")
+
+// MaxSize wraps an http.ResponseWriter and flushes every time more than size bytes are written.
+// If size is smaller than first write, it will never flush.
+type MaxSize struct {
+	writtenSinceLastFlush int
+
+	Size   int
+	Writer http.ResponseWriter
+}
+
+var _ http.ResponseWriter = (*MaxSize)(nil)
+
+func (maxSize *MaxSize) Write(b []byte) (n int, err error) {
+	n, err = maxSize.Writer.Write(b)
+	maxSize.writtenSinceLastFlush += n
+
+	if maxSize.writtenSinceLastFlush >= maxSize.Size {
+		f, ok := maxSize.Writer.(http.Flusher)
+		if !ok {
+			return 0, ErrWriterNotFlusher
+		}
+
+		f.Flush()
+		maxSize.writtenSinceLastFlush = 0
+	}
+
+	return n, err
+}
+
+func (maxSize MaxSize) Header() http.Header {
+	return maxSize.Writer.Header()
+}
+
+func (maxSize MaxSize) WriteHeader(statusCode int) {
+	maxSize.Writer.WriteHeader(statusCode)
+}
