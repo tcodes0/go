7e846729aa577425dcdecb362874941279f6de6a
Thom Ribeiro <thomazella9@gmail.com>
2024-04-27 13:07:13 -0300

    update workflows

---
+++ src/httpflush/test/max_size_test.go
@@ -0,0 +1,77 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/tcodes0/go/src/httpflush"
+)
+
+func Test_MaxSize_NoFlushSmaller(t *testing.T) {
+	assert := require.New(t)
+	writer := httpflush.NewMockresponseWriter(t)
+	maxSize := httpflush.MaxSize{
+		Max:    10,
+		Writer: writer,
+	}
+
+	writer.On("Write", mock.AnythingOfType("[]uint8")).Return(5, nil).Once()
+
+	n, err := maxSize.Write([]byte(""))
+	assert.Equal(4, n)
+	assert.NoError(err)
+}
+
+func Test_MaxSize_NoFlushEqual(t *testing.T) {
+	assert := require.New(t)
+	writer := httpflush.NewMockresponseWriter(t)
+	maxSize := httpflush.MaxSize{
+		Max:    10,
+		Writer: writer,
+	}
+
+	writer.On("Write", mock.AnythingOfType("[]uint8")).Return(10, nil).Once()
+
+	n, err := maxSize.Write([]byte(""))
+	assert.Equal(10, n)
+	assert.NoError(err)
+}
+
+func Test_MaxSize_FlushesLarger(t *testing.T) {
+	assert := require.New(t)
+	writer := httpflush.NewMockresponseWriter(t)
+	maxSize := httpflush.MaxSize{
+		Max:    10,
+		Writer: writer,
+	}
+
+	writer.On("Write", mock.AnythingOfType("[]uint8")).Return(20, nil).Once()
+	writer.On("Flush").Return(nil).Once()
+
+	n, err := maxSize.Write([]byte(""))
+	assert.Equal(20, n)
+	assert.NoError(err)
+}
+func Test_MaxSize_FlushesMany(t *testing.T) {
+	assert := require.New(t)
+	writer := httpflush.NewMockresponseWriter(t)
+	maxSize := httpflush.MaxSize{
+		Max:    10,
+		Writer: writer,
+	}
+
+	writer.On("Write", mock.AnythingOfType("[]uint8")).Return(20, nil).Once()
+	writer.On("Flush").Return(nil).Once()
+
+	n, err := maxSize.Write([]byte(""))
+	assert.Equal(20, n)
+	assert.NoError(err)
+
+	writer.On("Write", mock.AnythingOfType("[]uint8")).Return(11, nil).Once()
+	writer.On("Flush").Return(nil).Once()
+
+	n, err = maxSize.Write([]byte(""))
+	assert.Equal(11, n)
+	assert.NoError(err)
+}
