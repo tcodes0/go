90d03f8667977deb33d8303afe23e5fb1bff6d1e
Thom Ribeiro <thomazella9@gmail.com>
2024-04-30 05:53:36 -0300

    renames, package pr go test script

---
+++ src/sh/workflows/package-pr/go-test.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+fmt() {
+  if [ -n "$CI" ]; then
+    # on CI there is a step after this one that will process the tmpfile
+    tee "$(tmpfile)" | gotestfmt
+  else
+    gotestfmt
+  fi
+}
+
+# fail if any dependencies are missing
+flags+=(-mod=readonly)
+# output test results in json format for gotestfmt
+flags+=(-json)
+# detect race conditions
+flags+=(-race)
+# run all go vet checks, a linter
+flags+=(-vet=all)
+# disable passed test caching
+
+if [ "$CACHE" == "false" ]; then
+  flags+=(-count=1)
+fi
+
+tmpfile=$(mktemp)
+echo "$tmpfile"
+
+go test "${flags[@]}" "./$PKG/test" 2>&1 | fmt
+
+# a copy of test output is saved to a file for further processing in next workflow steps
+echo "tmpfile=$tmpfile" #>>"$GITHUB_OUTPUT"
