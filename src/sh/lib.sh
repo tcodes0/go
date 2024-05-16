b40669a218cd254312b0a39cc92fcddefe95a1b2
Thom Ribeiro <thomazella9@gmail.com>
2024-05-15 22:53:37 -0300

    tidy

--- src/sh/lib.sh
+++ src/sh/lib.sh
@@ -46,3 +46,46 @@ requireInternet() {
     msgExit "$message"
   fi
 }
+
+# run a test case and print the result
+testCase() {
+  local description=$1
+  local input=$2
+  local expected=$3
+  local result
+
+  # shellcheck disable=SC2086
+  if ! result=$($TESTEE $input); then
+    printf "%b\n" "$FAIL_RED $description"
+    printf "%b" "non zero exit"
+    exit 1
+  fi
+
+  if [ "$result" != "$expected" ]; then
+    printf "%b\n" "$FAIL_RED $description"
+    printf "%b\n" "expectation not met:"
+    printf "%b\n" "< expected"
+    diff <(printf %b "$expected") <(printf %b "$result")
+    exit 1
+  fi
+
+  printf "%b\n" "$PASS_GREEN $description"
+}
+
+# wait for all process to finish
+# example: wait 123 345 5665 3234
+wait() {
+  while true; do
+    doneCount=$#
+
+    for pid in "${@}"; do
+      if ! ps -p "$pid" >/dev/null; then
+        doneCount=$((doneCount - 1))
+      fi
+    done
+
+    if [ "$doneCount" = 0 ]; then
+      break
+    fi
+  done
+}
--- src/sh/test/tag-test.sh
+++ src/sh/test/tag-test.sh
@@ -7,59 +7,20 @@ shopt -s globstar
 # shellcheck disable=SC1091
 source "$PWD/src/sh/lib.sh"
 
-# run a test case and print the result
-run() {
-  local description=$1
-  local input=$2
-  local expected=$3
-  local result
-
-  # shellcheck disable=SC2086
-  if ! result=$($TESTEE $input); then
-    printf "%b\n" "$FAIL_RED $description"
-    printf "%b" "non zero exit"
-    exit 1
-  fi
-
-  if [ "$result" != "$expected" ]; then
-    printf "%b\n" "$FAIL_RED $description"
-    printf "%b\n" "expectation not met:"
-    printf "%b\n" "< expected"
-    diff <(printf %b "$expected") <(printf %b "$result")
-    exit 1
-  fi
-
-  printf "%b\n" "$PASS_GREEN $description"
-}
-
-# wait for all process to finish
-# example: wait 123 345 5665 3234
-wait() {
-  while true; do
-    doneCount=$#
-
-    for pid in "${@}"; do
-      if ! ps -p "$pid" >/dev/null; then
-        doneCount=$((doneCount - 1))
-      fi
-    done
-
-    if [ "$doneCount" = 0 ]; then
-      break
-    fi
-  done
-}
+### vars and functions ###
 
 export GIT_TAG="v1.2.3"
-export GIT_LOG="deadfaceb0 some commit message"
+export GIT_LOG="bada55c0ffe some commit message"
 export EXEC_GIT=./src/sh/test/mocks/git.sh
 export TESTEE=./src/sh/tag.sh
 testsRunning=()
 start=$(date +%s)
 
+### script ###
+
 msg "$(basename $TESTEE)" test
 
-run "increments major version" "major" "$(
+testCase "increments major version" "major" "$(
   cat <<EOF
 current	v1.2.3
 next	v2.0.0
@@ -67,7 +28,7 @@ EOF
 )" &
 testsRunning+=($!)
 
-run "increments minor version" "minor" "$(
+testCase "increments minor version" "minor" "$(
   cat <<EOF
 current	v1.2.3
 next	v1.3.0
@@ -75,7 +36,7 @@ EOF
 )" &
 testsRunning+=($!)
 
-run "increments patch version" "bump" "$(
+testCase "increments patch version" "bump" "$(
   cat <<EOF
 current	v1.2.3
 next	v1.2.4
@@ -84,7 +45,7 @@ EOF
 testsRunning+=($!)
 
 GIT_TAG="v1.2.3-pre22"
-run "increments patch pre release version" "bump" "$(
+testCase "increments pre release version" "bump" "$(
   cat <<EOF
 current	v1.2.3-pre22
 next	v1.2.3-pre23
@@ -93,7 +54,7 @@ EOF
 testsRunning+=($!)
 
 GIT_TAG="v1.2.3"
-run "major pre release" "major -p" "$(
+testCase "major pre release" "major -p" "$(
   cat <<EOF
 current	v1.2.3
 next	v2.0.0-pre1
@@ -101,7 +62,7 @@ EOF
 )" &
 testsRunning+=($!)
 
-run "minor pre release" "minor -p" "$(
+testCase "minor pre release" "minor -p" "$(
   cat <<EOF
 current	v1.2.3
 next	v1.3.0-pre1
@@ -109,7 +70,7 @@ EOF
 )" &
 testsRunning+=($!)
 
-run "patch pre release" "bump -p" "$(
+testCase "patch pre release" "bump -p" "$(
   cat <<EOF
 current	v1.2.3
 next	v1.2.4-pre1
@@ -118,7 +79,7 @@ EOF
 testsRunning+=($!)
 
 GIT_TAG="v1.2.3-pre1"
-run "patch already pre release" "bump -p" "$(
+testCase "patch already a pre release" "bump -p" "$(
   cat <<EOF
 current	v1.2.3-pre1
 next	v1.2.3-pre2
