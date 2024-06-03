d3ee46847652d224d9f684166227f29fe6f65343
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-06-03 15:32:38 -0400

    CI Commit lint job (#20)
    * ci(commitlint): add job
    
    * ci(commitlint): fix workflow
    
    * ci(commitlint): use a script instead of a marketplace action
    
    * chore(ci): cache node
    
    * chore(ci): fix npm install
    
    * chore(ci): fix npm install
    
    * chore(ci): fix npm install
    
    * chore(ci): commt lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script use head and base ref
    
    * chore(ci): commit lint script correct context vars locally
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(ci): commit lint script
    
    * chore(pr): code review

---
+++ sh/workflows/main/commit-lint.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env bash
+
+### options, imports, mocks ###
+
+set -euo pipefail
+shopt -s globstar
+
+### vars and functions ###
+
+lintCommits() {
+  local log="$1" problems=()
+
+  while read -r commit; do
+    out="$(commitlint --config="$CONFIG_PATH" <<<"$commit" || true)"
+
+    if [ -n "$out" ]; then
+      problems+=("$out")
+    fi
+  done <<<"$log"
+
+  printf %s "${problems[*]}"
+}
+
+### validation, input handling ###
+
+if [ -z "$BASE_REF" ]; then
+  echo "BASE_REF is empty"
+  exit 1
+fi
+
+### script ###
+
+if ! command -v commitlint >/dev/null; then
+  npm install --global @commitlint/cli@"$VERSION" >/dev/null
+fi
+
+revision=refs/remotes/origin/"$BASE_REF"..HEAD
+
+echo git log "$revision"
+
+log=$(git log --format=%s "$revision" --)
+issues=$(lintCommits "$log")
+
+if [ -n "$issues" ]; then
+  totalCommits=$(wc -l <<<"$log")
+  badCommits=$(grep -Eie input -c <<<"$issues")
+
+  echo commits:
+  echo "$log"
+  echo
+  echo linter\ output:
+  echo
+  echo "$issues"
+  echo
+  echo "Commit messages not formatted properly: $badCommits out of $totalCommits commits"
+  echo "See https://www.conventionalcommits.org/en/v1.0.0/"
+  echo "To fix all, try 'git rebase -i $revision', change bad commits to 'reword', fix messages and 'git push --force'"
+  exit 1
+fi
--- sh/workflows/main/diff.sh
+++ sh/workflows/main/diff.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env bash
 
+### options, imports, mocks ###
+
 set -euo pipefail
 shopt -s globstar
 
+### vars and functions ###
+
+### validation, input handling ###
+
+### script ###
+
 diff=$(git diff .)
+
 if [ -n "$diff" ]; then
   echo "$diff"
   echo "update files and commit changes"
