2d08b0c6e2af9c4894c82f8b8a68735bf3ba28d2
Raphael Thom Thomazella <thomazella9@gmail.com>
2024-06-03 13:36:55 -0300

    chore(ci): commt lint script

--- sh/workflows/main/commit-lint.sh
+++ sh/workflows/main/commit-lint.sh
@@ -7,12 +7,44 @@ shopt -s globstar
 
 ### vars and functions ###
 
+lintCommits() {
+  local log="$1" problems=() out
+
+  while read -r commit; do
+    out="$(commitlint --config="$CONFIG_PATH" <<<"$commit" || true)"
+
+    if [ -n "$out" ]; then
+      problems+=("$out")
+    fi
+  done <<<"$log"
+
+  printf %s "${problems[*]}"
+}
+
 ### validation, input handling ###
 
 ### script ###
 
 if ! command -v commitlint >/dev/null; then
-  npm install --global @commitlint/cli@"$VERSION"
+  npm install --global @commitlint/cli@"$VERSION" >/dev/null
 fi
 
-commitlint --config="$CONFIG_PATH" <<<"$(git log --format=%B -n 1 HEAD)"
+log=$(git log --format=%s origin/main..HEAD)
+issues=$(lintCommits "$log")
+
+if [ -n "$issues" ]; then
+  totalCommits=$(wc -l <<<"$log")
+  badCommits=$(grep -Eie input -c <<<"$issues")
+
+  echo commits:
+  echo "$log"
+  echo
+  echo linter\ output:
+  echo
+  echo "$issues"
+  echo
+  echo "Commit messages not formatted properly: $badCommits out of $totalCommits commits"
+  echo "See https://www.conventionalcommits.org/en/v1.0.0/"
+  echo "To fix all, try 'git rebase -i origin/main..HEAD', change bad commits to 'reword', fix messages and 'git push --force'"
+  exit 1
+fi
