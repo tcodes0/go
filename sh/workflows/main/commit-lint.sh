50ae48b2dedb6140c23fcb9304b9d890331cb0d9
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-06-04 08:50:52 -0400

    feat(ci): new checks & improvements to commitlint (#23)
    * fix(commitlint): success when running on main
    
    * feat(ci): add new go mod, go work checks to root and modules
    
    * refactor(main-workflow): extract changed file globs to a file
    
    * feat(ci): check pr title to be a conventional commit
    
    * chore(ci): make commitlint job more verbose

--- sh/workflows/main/commit-lint.sh
+++ sh/workflows/main/commit-lint.sh
@@ -7,6 +7,8 @@ shopt -s globstar
 
 ### vars and functions ###
 
+CONVENTIONAL_COMMITS_URL="See https://www.conventionalcommits.org/en/v1.0.0/"
+
 lintCommits() {
   local log="$1" problems=()
 
@@ -23,9 +25,8 @@ lintCommits() {
 
 ### validation, input handling ###
 
-if [ -z "$BASE_REF" ]; then
-  echo "BASE_REF is empty"
-  exit 1
+if [ -z "${BASE_REF:-}" ]; then
+  BASE_REF=main
 fi
 
 ### script ###
@@ -34,11 +35,29 @@ if ! command -v commitlint >/dev/null; then
   npm install --global @commitlint/cli@"$VERSION" >/dev/null
 fi
 
+echo "$PR_TITLE"
+
+if ! commitlint --config="$CONFIG_PATH" <<<"$PR_TITLE"; then
+  echo "PR title must be a conventional commit, got: $PR_TITLE"
+  echo "$CONVENTIONAL_COMMITS_URL"
+  exit 1
+fi
+
+echo "PR title ok"
+
 revision=refs/remotes/origin/"$BASE_REF"..HEAD
 
 echo git log "$revision"
 
 log=$(git log --format=%s "$revision" --)
+
+if [ -z "$log" ]; then
+  echo "empty git log"
+  exit
+fi
+
+echo "$log"
+
 issues=$(lintCommits "$log")
 
 if [ -n "$issues" ]; then
@@ -53,7 +72,9 @@ if [ -n "$issues" ]; then
   echo "$issues"
   echo
   echo "Commit messages not formatted properly: $badCommits out of $totalCommits commits"
-  echo "See https://www.conventionalcommits.org/en/v1.0.0/"
+  echo "$CONVENTIONAL_COMMITS_URL"
   echo "To fix all, try 'git rebase -i $revision', change bad commits to 'reword', fix messages and 'git push --force'"
   exit 1
 fi
+
+echo "commits ok"
