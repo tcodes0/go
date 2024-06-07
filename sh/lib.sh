eeee78a606a0c8dbddf74595a8d68ab3fece298b
Thom Ribeiro <thomazella9@gmail.com>
2024-06-07 18:58:10 -0300

    chore(ci): run copyright

--- sh/lib.sh
+++ sh/lib.sh
@@ -112,7 +112,8 @@ requireGitBranch() {
 # outputs one module per line
 findModules() {
   regExpTestSuffix="test$"
-  find . -mindepth 2 -maxdepth 3 -type f -name '*.go' -exec dirname {} \; | _sed --regexp-extended -e "/$regExpTestSuffix/d" | sort --stable | uniq
+  regExpLocalSuffix=".local$"
+  find . -mindepth 2 -maxdepth 3 -type f -name '*.go' -exec dirname {} \; | _sed --regexp-extended -e "/$regExpTestSuffix/d" -e "/$regExpLocalSuffix/d" | sort --stable | uniq
 }
 
 # find folders directly under . that have at least one *.go file and prettify output
--- sh/run.sh
+++ sh/run.sh
@@ -19,47 +19,28 @@ read -rd "$(printf \\r)" -a modules < <(
 )
 
 declare -ra commands=(
-  "name:build               type:mod  argCount:2"
-  "name:format              type:mod  argCount:1"
-  "name:lint                type:mod  argCount:1"
-  "name:lintFix             type:mod  argCount:1"
+  "name:build               type:mod"
+  "name:format              type:mod"
+  "name:lint                type:mod"
+  "name:lintFix             type:mod"
   # do not name "test"; shadowed by builtin test command
-  "name:tests               type:mod  argCount:2"
-  "name:ci                  type:repo argCount:0"
-  "name:ciPush              type:repo argCount:0"
-  "name:coverage            type:repo argCount:0"
-  "name:formatConfigs        type:repo argCount:0"
-  "name:generateMocks       type:repo argCount:0"
-  "name:spellcheckDocs      type:repo argCount:0"
-  "name:setup               type:repo argCount:0"
-  "name:tag                 type:repo argCount:2"
-  "name:testScripts         type:repo argCount:0"
-  "name:generateGoWork      type:repo argCount:0"
-  "name:newModule           type:repo argCount:1"
-  "name:updateVscodeConfigs  type:repo argCount:0"
+  "name:tests               type:mod"
+  "name:ci                  type:repo"
+  "name:ciPush              type:repo"
+  "name:coverage            type:repo"
+  "name:formatConfigs        type:repo"
+  "name:generateMocks       type:repo"
+  "name:spellcheckDocs      type:repo"
+  "name:setup               type:repo"
+  "name:tag                 type:repo"
+  "name:testScripts         type:repo"
+  "name:generateGoWork      type:repo"
+  "name:newModule           type:repo"
+  "name:updateVscodeConfigs  type:repo"
+  "name:cmd/copyright       type:repo"
 )
 
 usageExit() {
-  cmdUsage() {
-    local name=$1 type=$2 argCount=$3
-
-    msg "$0" "$name"
-
-    if [ "$argCount" != 0 ]; then
-      printf \\t
-
-      for ((i = 1; i <= argCount; i++)); do
-        if [ "$type" == mod ] && [ "$i" == 1 ]; then
-          printf "<module>\t"
-        else
-          printf "<arg%s>\t" $i
-        fi
-      done
-    fi
-
-    printf \\n
-  }
-
   local regExpInvalidCapture='.nvalid\ [^:]+:\ ([[:alpha:]]*)' cmdNames=()
 
   msgln "$*"
@@ -70,12 +51,19 @@ usageExit() {
 
     cmdNames+=("${command[0]/name:/}")
 
-    cmdUsage "${command[0]/name:/}" "${command[1]/type:/}" "${command[2]/argCount:/}"
+    msg "$0" "${command[0]/name:/}"
+
+    if [ "${command[1]/type:/}" == mod ]; then
+      printf "\t<module>"
+    fi
+
+    printf \\n
   done
 
   printf \\n
   msgln "modules: $(joinBy ', ' "${modules[@]}")"
   msgln use \'all\' as module to iterate all modules
+  msgln pass -h to commands to see further options
 
   if [[ "$*" =~ $regExpInvalidCapture ]]; then
     didYouMean "${BASH_REMATCH[1]}" "${modules[@]}" "${cmdNames[@]}"
@@ -152,7 +140,7 @@ formatConfigs() {
 tests() {
   displayCoverage=""
 
-  if [ "${inputArgs[1]}" == -cover ]; then
+  if [ "${inputArgs[1]:-}" == -cover ]; then
     displayCoverage=true
   fi
 
@@ -167,7 +155,7 @@ tests() {
 build() {
   install=""
 
-  if [ "${inputArgs[1]}" == -install ]; then
+  if [ "${inputArgs[1]:-}" == -install ]; then
     install=true
   fi
 
@@ -249,6 +237,11 @@ ciPush() {
   ./sh/ci.sh push
 }
 
+# shellcheck disable=SC2317 # dynamic call
+cmd/copyright() {
+  ./sh/workflows/main/run-copyright.sh "$@"
+}
+
 ### validation, input handling ###
 
 if [ $# -lt 1 ]; then
@@ -272,10 +265,6 @@ for info in "${commands[@]}"; do
     "${command[0]/name:/}" "${inputArgs[@]}"
     exit 1
   fi
-
-  # if [ "${#inputArgs[@]}" != "${command[2]/argCount:/}" ]; then
-  #   usageExit "${command[0]/name:/} wants ${command[2]/argCount:/} arguments; received ${#inputArgs[@]} (${inputArgs[*]})"
-  # fi
 
   if [ "${command[1]/type:/}" == mod ]; then
     if ! [[ " all ${modules[*]} " =~ ${inputArgs[0]} ]]; then
