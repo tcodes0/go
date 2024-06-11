53228ad359704c21a3cdbb15238efaed19610493
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-06-11 01:42:30 -0300

    refactor(copyright): further simplify main

--- copyright/main.go
+++ copyright/main.go
@@ -6,11 +6,11 @@
 package main
 
 import (
-	"bufio"
 	_ "embed"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -23,26 +23,24 @@ import (
 
 var (
 	//go:embed header.txt
-	header   string
-	headerGo string
-	headerSh string
-	flagset  = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	header    string
+	headerGo  string
+	headerSh  string
+	commentGo = "// "
+	commentSh = "# "
+	flagset   = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	findNames = []string{"*.go", "*.sh"}
+	ignore    = regexp.MustCompile("/?mock_.*|.local/.*")
 )
 
 func main() {
 	fLogLevel := flagset.Int("log-level", int(logging.LInfo), "control logging output; 1 is debug, the higher the less logs.")
 	fColor := flagset.Bool("color", false, "colored logging output. (default false)")
 	fFix := flagset.Bool("fix", false, "applies header to files. (default false)")
-	fGlobs := flagset.String("globs", "", "comma-space separated list of globs to search for files. (required)")
-	fIgnore := flagset.String("ignore", "", "comma-space separated list of regexes to exclude files by path match. (default empty)")
 
 	err := flagset.Parse(os.Args[1:])
 	if err != nil {
 		usageExit(err)
-	}
-
-	if *fGlobs == "" {
-		usageExit(errors.New("globs required"))
 	}
 
 	opts := []logging.CreateOptions{logging.OptFlags(log.Lshortfile), logging.OptLevel(logging.Level(*fLogLevel))}
@@ -51,103 +49,42 @@ func main() {
 	}
 
 	logger := logging.Create(opts...)
-	globs := strings.Split(*fGlobs, ", ")
-	rawRegexps := strings.Split(*fIgnore, ", ")
 
 	if *fLogLevel == 0 && !*fFix {
+		// withut -fix and explicit level only print errors
 		logger.SetLevel(logging.LError)
-	}
-
-	ignores := make([]*regexp.Regexp, 0, len(rawRegexps))
-
-	for _, raw := range rawRegexps {
-		var reg *regexp.Regexp
-
-		if raw == "" {
-			continue
-		}
-
-		reg, err = regexp.Compile(raw)
-		if err != nil {
-			logger.Fatalf("compiling regexp %s: %v", raw, err)
-		}
-
-		ignores = append(ignores, reg)
 	}
 
 	headerLines := strings.Split(header, "\n")
 
 	for _, line := range headerLines {
-		headerGo += "// " + line + "\n"
-		headerSh += "# " + line + "\n"
+		headerGo += commentGo + line + "\n"
+		headerSh += commentSh + line + "\n"
 	}
 
-	err = boilerplate(*logger, globs, ignores, *fFix)
+	err = boilerplate(*logger, *fFix)
 	if err != nil {
 		logger.Fatalf("fatal: %v", err)
 	}
 }
 
 func usageExit(err error) {
+	fmt.Println()
 	fmt.Println("Check and fix missing boilerplate header in files")
 	fmt.Println("Without -fix fails if files are missing copyright header and prints files")
 	fmt.Println()
-	flagset.Usage()
 
-	if err != nil {
+	if err != nil && !errors.Is(err, flag.ErrHelp) {
 		fmt.Printf("error: %v\n", err)
 	}
 
 	os.Exit(1)
 }
 
-func boilerplate(logger logging.Logger, globs []string, ignoreRegexps []*regexp.Regexp, fix bool) error {
-	var paths []string
-
-	for _, fileglob := range globs {
-		cmd := exec.Command("find", ".", "-name", fileglob, "-type", "f")
-
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			return misc.Wrapf(err, "find %s", fileglob)
-		}
-
-		filePaths := strings.Split(string(output), "\n")
-		logger.Debug().Logf("glob: '%s', count %d, files: %s", fileglob, len(filePaths), filePaths)
-
-		if len(filePaths) == 0 {
-			logger.Warn().Logf("no files matched: %s", fileglob)
-
-			continue
-		}
-
-	filePathsLoop:
-		for _, filePath := range filePaths {
-			if filePath == "" {
-				continue
-			}
-
-			for _, regexp := range ignoreRegexps {
-				if regexp.MatchString(filePath) {
-					logger.Debug().Logf("skip %s ignore '%s'", filePath, regexp.String())
-
-					continue filePathsLoop
-				}
-			}
-
-			hasHeader, err := detectHeader(filePath)
-			if err != nil {
-				return misc.Wrap(err, "detecting header")
-			}
-
-			if hasHeader {
-				logger.Debug().Logf("ok: %s", filePath)
-
-				continue
-			}
-
-			paths = append(paths, filePath)
-		}
+func boilerplate(logger logging.Logger, fix bool) error {
+	paths, err := filesWithoutHeader(logger)
+	if err != nil {
+		return misc.Wrap(err, "finding")
 	}
 
 	if len(paths) == 0 {
@@ -165,63 +102,92 @@ func boilerplate(logger logging.Logger, globs []string, ignoreRegexps []*regexp.
 	for _, path := range paths {
 		err := fixFile(path)
 		if err != nil {
-			return misc.Wrapf(err, "processing %s", path)
+			return misc.Wrapf(err, "fixing %s", path)
 		}
 	}
 
 	return nil
 }
 
-func detectHeader(path string) (hasHeader bool, err error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return false, misc.Wrap(err, "opening file")
-	}
+func filesWithoutHeader(logger logging.Logger) (paths []string, err error) {
+	for _, findName := range findNames {
+		cmd := exec.Command("find", ".", "-name", findName, "-type", "f")
 
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-
-	head := ""
-
-	for range 2 {
-		ok := scanner.Scan()
-		if !ok {
-			return false, misc.Wrap(scanner.Err(), "scanning")
+		findOut, err := cmd.CombinedOutput()
+		if err != nil {
+			return nil, misc.Wrapf(err, "find %s", findName)
 		}
 
-		head += scanner.Text()
+		filesFound := strings.Split(string(findOut), "\n")
+		logger.Debug().Logf("find: '%s', count %d, files: %s", findName, len(filesFound), filesFound)
+
+		if len(filesFound) == 0 {
+			logger.Warn().Logf("no matches: %s", findName)
+
+			continue
+		}
+
+		for _, filePath := range filesFound {
+			if filePath == "" {
+				continue
+			}
+
+			if ignore.MatchString(filePath) {
+				logger.Debug().Logf("ignored %s", filePath)
+
+				continue
+			}
+
+			fileHeader, err := exec.Command("head", filePath).CombinedOutput()
+			if err != nil {
+				return nil, misc.Wrapf(err, "head %s", filePath)
+			}
+
+			// not specific on purpose, header may change if file is from another oss project
+			if strings.Contains(string(fileHeader), "Copyright") {
+				logger.Debug().Logf("ok: %s", filePath)
+
+				continue
+			}
+
+			paths = append(paths, filePath)
+		}
 	}
 
-	return strings.Contains(head, "Copyright"), nil
+	return paths, nil
 }
 
 func fixFile(path string) error {
 	file, err := os.OpenFile(path, os.O_RDWR, 0)
 	if err != nil {
-		return misc.Wrap(err, "opening file for write")
+		return misc.Wrap(err, "opening")
 	}
 
 	defer file.Close()
 
-	if strings.Contains(path, ".go") {
-		_, err = file.WriteAt([]byte(headerGo), 0)
-	} else if strings.Contains(path, ".sh") {
-		scanner := bufio.NewScanner(file)
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return misc.Wrap(err, "reading")
+	}
 
-		ok := scanner.Scan()
-		if !ok {
-			return misc.Wrap(scanner.Err(), "scanning")
+	var newFile string
+
+	if strings.Contains(path, ".go") {
+		newFile = headerGo + "\n" + string(content)
+	} else if strings.Contains(path, ".sh") {
+		shebang, rest, found := strings.Cut(string(content), "\n")
+		if !found {
+			return fmt.Errorf("detecting shebang: %s", path)
 		}
 
-		// scanner.text contains the first line of the sh file, the shebang
-		// write copyright header after
-		_, err = file.WriteAt([]byte("\n"+headerSh), int64(len(scanner.Text())))
+		newFile = shebang + "\n" + headerSh + rest
 	} else {
 		return fmt.Errorf("unknown file type: %s", path)
 	}
 
+	_, err = file.WriteAt([]byte(newFile), 0)
 	if err != nil {
-		return misc.Wrap(err, "writing to file")
+		return misc.Wrap(err, "writing")
 	}
 
 	return nil
