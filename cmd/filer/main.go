34481c889ba00bffd3189392e927a1b55c67a888
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-06-19 22:33:55 -0300

    chore(cmd/filer): init mod

---
+++ cmd/filer/main.go
@@ -0,0 +1,77 @@
+// Copyright 2024 Raphael Thomazella. All rights reserved.
+// Use of this source code is governed by the BSD-3-Clause
+// license that can be found in the LICENSE file and online
+// at https://opensource.org/license/BSD-3-clause.
+
+package main
+
+import (
+	_ "embed"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tcodes0/go/cmd"
+	"github.com/tcodes0/go/logging"
+	"github.com/tcodes0/go/misc"
+	"gopkg.in/yaml.v3"
+)
+
+type config struct {
+	Food string `yaml:"food"`
+}
+
+var (
+	//go:embed config.yml
+	raw     string
+	configs config
+	flagset = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+)
+
+func main() {
+	_ = flagset.Bool("pizza", true, "pepperoni or mozzarella!. (default TRUE)")
+
+	err := flagset.Parse(os.Args[1:])
+	if err != nil {
+		usageExit(err)
+	}
+
+	err = yaml.Unmarshal([]byte(raw), &configs)
+	if err != nil {
+		usageExit(err)
+	}
+
+	fColor := misc.LookupEnv(cmd.EnvColor, false)
+	fLogLevel := misc.LookupEnv(cmd.EnvLogLevel, int(logging.LInfo))
+
+	opts := []logging.CreateOptions{logging.OptFlags(log.Lshortfile), logging.OptLevel(logging.Level(fLogLevel))}
+	if fColor {
+		opts = append(opts, logging.OptColor())
+	}
+
+	logger := logging.Create(opts...)
+
+	err = filer(*logger)
+	if err != nil {
+		logger.Fatalf("fatal: %v", err)
+	}
+}
+
+func usageExit(err error) {
+	fmt.Println()
+	fmt.Println("description here")
+	fmt.Println()
+	fmt.Println(cmd.EnvVarUsage())
+
+	if err != nil && !errors.Is(err, flag.ErrHelp) {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	os.Exit(1)
+}
+
+func filer(_ logging.Logger) error {
+	return nil
+}
