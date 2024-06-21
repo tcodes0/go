f5e2938cbe807d66c478d5d665725f4b5e8c06cd
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-06-21 19:16:43 -0300

    chore: renames

--- misc/signal.go
+++ misc/signal.go
@@ -12,9 +12,9 @@ import (
 	"syscall"
 )
 
-// catches system signals like interrupt, hang up and terminate once and calls
-// the handler function.
-func ListenStopSignal(ctx context.Context, handler func(sig os.Signal)) {
+// blocks until receiving interrupt, hang up or terminate; then calls
+// the handler function once.
+func RoutineListenStopSignal(ctx context.Context, handler func(sig os.Signal)) {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 
