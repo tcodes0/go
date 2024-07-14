cfe6266328df800e4d69f5b9c2e23fe5b258ff87
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-07-13 23:20:01 -0300

    feat(workflows): add release workflow to push tag on release PR merge

---
+++ sh/workflows/release/tag.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+# Copyright 2024 Raphael Thomazella. All rights reserved.
+# Use of this source code is governed by the BSD-3-Clause
+# license that can be found in the LICENSE file and online
+# at https://opensource.org/license/BSD-3-clause.
+
+set -euo pipefail
+shopt -s globstar
+# shellcheck disable=SC1091
+source "$PWD/sh/lib.sh"
+trap 'err $LINENO' ERR
+
+### vars and functions ###
+
+changelog_file="CHANGELOG.md"
+
+### script ###
+
+if ! [[ $(git log --oneline --decorate | head -1) =~ chore:\ release ]]; then
+  log "main head not a release commit"
+  exit 0
+fi
+
+if ! [[ $(head -1 "$changelog_file") =~ \#\ ([[:alnum:]]+):\ (v.+\..+\.[[:digit:]]+) ]]; then
+  err "malformed changelog head"
+  exit 1
+fi
+
+tag="${BASH_REMATCH[1]}/${BASH_REMATCH[2]}"
+msgln "$tag"
+git tag "$tag" HEAD
+git push origin --tags
