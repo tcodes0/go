11d7a1d7a4723442d0efaf2466b40fc0ed0027da
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2024-08-23 13:38:55 -0300

    feat(workflows): update release automation

--- sh/workflows/release/changelog.sh
+++ sh/workflows/release/changelog.sh
@@ -3,15 +3,24 @@
 # Use of this source code is governed by the BSD-3-Clause
 # license that can be found in the LICENSE file and online
 # at https://opensource.org/license/BSD-3-clause.
+#
+# wrapper around changelog tool.
+# Args: 1=url 2=title 3=prefix
 
 set -euo pipefail
 shopt -s globstar
 # shellcheck source=../../lib.sh
-source "$PWD/sh/lib.sh"
+source "$PWD/lib.sh"
 trap 'err $LINENO' ERR
 
+##########################
 ### vars and functions ###
+##########################
 
+# Description: Validates user input
+# Globals    : CHANGELOG_FILE (github workflow or .env)
+# Returns    : 1 if fail
+# Example    : validate
 validate() {
   if [ ! -f "$CHANGELOG_FILE" ]; then
     err $LINENO "$CHANGELOG_FILE" not found
@@ -19,13 +28,22 @@ validate() {
   fi
 }
 
-updateChangelog() {
-  local module=$1 changes changelog flags=()
+# Description: Calls changelog tool and updates CHANGELOG_FILE
+# Globals    : CHANGELOG_FILE (github workflow or .env)
+# Args       : 1=url 2=title 3=prefix
+# STDERR     : Might print errors
+# Returns    : 1 if fail
+# Sideeffects: Updates CHANGELOG_FILE
+# Example    : update_changelog pizza
+update_changelog() {
+  local url=$1 title=${2:-} prefix=${3:-} changes changelog flags=()
 
   changelog=$(cat "$CHANGELOG_FILE")
-  flags+=(-module "$module")
+  flags+=(-title "$title")
+  flags+=(-tagprefix "$prefix")
+  flags+=(-url "$url")
 
-  changes=$(go run ./cmd/changelog/main.go "${flags[@]}")
+  changes=$(changelog "${flags[@]}")
   if [ ! "$changes" ]; then
     err $LINENO "empty changes"
     return 1
@@ -35,9 +53,14 @@ updateChangelog() {
   printf %s "$changelog" >>"$CHANGELOG_FILE"
 }
 
+##############
 ### script ###
+##############
 
-module=$1
+if [ ! "${CHANGELOG_FILE-}" ]; then
+  # shellcheck source=../../.env
+  source .env
+fi
 
 validate
-updateChangelog "$module"
+update_changelog "$@"
